1c6ac1c8d11242f14312dffca4486e54971025b6
Com1 Software, Inc <information@com1software.com>
2022-10-26 16:14:05 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,16 +7,12 @@ import (
 )
 
 func main() {
-	d, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x39)
+	x:=[]byte{}
+	d, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x1e)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("first return %s", d)
-	// opens a 10-bit address
-	d, err = i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, i2c.TenBit(0x78))
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("to bit return %s", d)
-
+	fmt.Printf("Open %s\n", d)
+	d.Read(x)
+	fmt.Println(x)
 }
